53012a85abaa558965ff799d3b41d6f5ffdc6beb
Niklas <contact@niklash.dev>
2023-02-13 17:33:46 +0000

    fix: remove secret and genereate on demand

--- openinv/settings.py
+++ openinv/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+from decouple import config
+
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+try:
+    if config("SECRET_KEY") == "":
+        pass
+except:
+    Key = get_random_secret_key()
+    os.environ["SECRET_KEY"] = Key
+    with open(".env", "w") as f:
+        f.write(f"SECRET_KEY={Key}")
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h5%ivn##^jj1-^98$+5&6m(0f34=wgiz*)!4q0#)9$+!8fgnft"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
